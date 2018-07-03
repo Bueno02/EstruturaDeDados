6b3c447a0e055f8f43035f952791469e1623d6a6
Bueno02 <40775306+Bueno02@users.noreply.github.com>
2018-07-02 23:43:26 -0300

    Update trabalho.cpp

--- trabalho.cpp
+++ trabalho.cpp
@@ -62,9 +62,8 @@ void peneira (int m, int *v);
 
 void selectionSort(int vet[], long int tam,dado *dados);
 
-void chamarquicksort(int* vet, int inicio, int fim ,dado *dados);
+void chamarquicksort(int* vet, int inicio,long int fim ,dado *dados);
 
-void quicksort (int* vet, int inicio, int fim );
 
 void bubblesort(int vet[],long int n,dado *dados);
 
@@ -168,12 +167,12 @@ int main(){
 
 	  selectionSort(Vet1,tam,dados);
 	  
-	  chamarquicksort(Vet2,0,tam,dados);
+	  chamarquicksort(Vet2,0,tam-1,dados);
 	  
 	  bubblesort(Vet3,tam,dados);
 
 
-
+system("pause");
 
 	  
 
@@ -332,48 +331,47 @@ void selectionSort(int vet[],long int tam,dado *dados){
 }
 
 
-int particiona(int* vet, int inicio, int fim){
-	int dir,esq,aux,pivo;
-	esq=inicio;dir=fim;pivo=vet[inicio];
-	
-	while(esq<dir){
-		while(vet[esq]<=pivo){esq++;
-		}
-		while(vet[dir]>pivo){dir--;
-		}
-		if(esq<dir){
-			aux=vet[esq];
-			vet[esq]=vet[dir];
-			vet[dir]=aux;
 
-		}
-	}
 
-	vet[inicio]=vet[dir];
-	vet[dir]=pivo;
-
-	return dir;
+void quick_sort(int *a, int left, int right) {
+    int i, j, x, y;
+     
+    i = left;
+    j = right;
+    x = a[(left + right) / 2];
+     
+    while(i <= j) {
+        while(a[i] < x && i < right) {
+            i++;
+        }
+        while(a[j] > x && j > left) {
+            j--;
+        }
+        if(i <= j) {
+            y = a[i];
+            a[i] = a[j];
+            a[j] = y;
+            i++;
+            j--;
+        }
+    }
+     
+    if(j > left) {
+        quick_sort(a, left, j);
+    }
+    if(i < right) {
+        quick_sort(a, i, right);
+    }
 }
 
-
-void quicksort (int* vet, int inicio, int fim){
-
-	int pivo;
-	if(fim>inicio){
-		pivo = particiona (vet,inicio, fim);
-		quicksort(vet, inicio, pivo-1);
-		quicksort(vet,pivo+1,fim);
-	}
-}
-
-void chamarquicksort(int* vet, int inicio, int fim,dado *dados){
+void chamarquicksort(int* vet, int inicio,long int fim,dado *dados){
 	printf("\nIniciando QuickSort - Aguarde...\n");
 	
 	clock_t t0,tf;
 
 	t0=clock();
 	
-	quicksort(vet,inicio,fim);
+	quick_sort(vet,inicio,fim);
 	
 	tf=clock();
 
